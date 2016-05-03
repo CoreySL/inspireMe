fb9ae66bc2c0360d84687ba40d0c51e1d226611a
CoreySL <cscottlin@gmail.com>
2016-05-03 16:13:26 -0700

    Added image, jquery fade effect, and spacebar keydown effect to change quote.

--- default.js
+++ default.js
@@ -1,32 +1,38 @@
+var getQuote = function() {
+  $.ajax( {
+    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?',
+    dataType: 'jsonp',
+    success: function(data) {
+      var post = data.shift();
+      if (post.content.length > 75) {
+        getQuote();
+      }
+      else {
+        var postTitle = post.title;
+        var dashTitle = '- ' + post.title;
+        $('#quote-title').fadeOut('100', function() {
+          $('#quote-title').html(dashTitle).fadeIn('100');
+        })
+        var content = post.content;
+        var sliceP1 = content.slice(3, content.length);
+        var sliceP2 = sliceP1.slice(0, sliceP1.length - 5);
+        var quotedContent = '"' + sliceP2 + '"';
+        $('#quote-content').fadeOut(function() {
+          $('#quote-content').html(quotedContent).fadeIn('100');
+        })
+      }
+    },
+    cache: false
+  });
+}
+
 $(document).ready(function() {
-//   $('#get-another-quote-button').on('click', function() {
-//     $.ajax({
-//
-//       // $('#quote-div').text(post.title);
-//     });
-//     $('#quote-div').html(post.content);
-//
-//   });
-// });
+  $(document).keydown(function(e) {
+    // e.preventDefault();
+    if(e.keyCode == 32){
+       // user has pressed space
+       getQuote();
 
-  $('#get-another-quote-button').on('click', function(e) {
-    e.preventDefault();
-    $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?',
-      dataType: 'jsonp',
-      success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-
-        // // If the Source is available, use it. Otherwise hide it.
-        // if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-        //   $('#quote-source').html('Source:' + post.custom_meta.Source);
-        // } else {
-        //   $('#quote-source').text('');
-        // }
-      },
-      cache: false
-    });
+    }
   });
 });
